4d535aa289eefcf26e37434445b48024058e2e2d
sania-jay <92186934+sania-jay@users.noreply.github.com>
2022-01-24 13:13:06 +0000

    added rel="noreferrer"

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import Dictionary from './Dictionary';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+      <header className="App-header"></header>
 
       <main>
         <Dictionary />
       </main>
       <footer className="App-Footer">
-        <small>Coded by Sania, open sourced on <a href="https://github.com/sania-jay/dictionary" target="_blank">GitHub</a></small>
+        <small>
+          Coded by Sania, open sourced on{" "}
+          <a
+            href="https://github.com/sania-jay/dictionary"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </small>
       </footer>
     </div>
   );
